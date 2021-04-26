02346e704cd8d077d34f513074df47cc36c77774
saeedmuhammad2013@gmail.com <saeed924178@gmail.com>
2021-04-26 14:08:15 +0100

    posting with postman

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const app = express();
+const bodyparser = require('body-parser');
 
-app.post("/",(req,res)=>{
+app.use(bodyparser.json());
+
+app.post("/",(req,res) => {
 
   var email = req.body.email;
   var amount = req.body.amount;
 
-  res.send({"amount : " amount,"email : "email});
+  res.send({"amount " : amount,"email  " : email});
 
-})
+});
 
 app.listen(3000,()=>{
 
  console.log('server is running on port 3000');
 
-})
+});
